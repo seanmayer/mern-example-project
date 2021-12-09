375341cbb091cb2bcaf1974241ee45a65a7beca3
Sean Mayer <2008sean@live.com>
2021-12-09 06:27:06 +0000

    Adding UserList and User Item
    - Adding a UserList Page Component
    - Adding a UserItem Component
    - Added missing node package dependencies to project

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+        {
+            id:'u1',
+            name:'Sean',
+            image:
+                'https://scaleablethinking.files.wordpress.com/2021/12/498ba7d2-fd7c-46b5-af73-980efd3903e9_1_201_a.jpeg',
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS}/>;
 };
 
 export default Users;
