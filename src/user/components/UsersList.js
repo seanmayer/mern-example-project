97b5b9a88da2052a9fa7a420942a487b57126503
Sean Mayer <2008sean@live.com>
2021-12-11 17:36:45 +0000

    Adding MainHeader
    - resizable navigation bar

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
     if(props.items.length === 0) {
         return (
-        <div className="center">
-            <h2>No users found.</h2>
-        </div>
+          <div className="center">
+            <Card>
+              <h2>No users found.</h2>
+            </Card>
+          </div>
         );
     };
 
