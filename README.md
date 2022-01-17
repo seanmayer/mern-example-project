6c78e60ebf39905751b68bad0da6955625775ce4
Sean Mayer <2008sean@live.com>
2022-01-17 06:43:22 +0000

    Setting up ExpressJS

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mern-example-project
 Building a fullstack React.js application with Node.js, Express.js &amp; MongoDB (MERN)
+
+# Setting up NodeJS with express
+
+Install express -> `npm install express --save --prefix nodejs`
+
+Run the server -> `node nodejs/app.js`
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,28 @@
-const userName = "sean";
+const express = require('express');
 
-console.log(userName);
+const app = express();
+
+app.use((req,res,next) => {
+    let body = '';
+    req.on('end', () => {
+        const userName = body.split("=")[1];
+        if (userName) {
+            req.body = { name: userName };
+        }
+        next();
+    });
+    
+    req.on('data', chunk => {
+        body += chunk;
+    });
+});
+
+app.use((req, res, next) => {
+    if(req.body) {
+        return res.send('<h1>' + req.body.name + '</h1>');
+    }
+
+    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
+});
+
+app.listen(5000);
