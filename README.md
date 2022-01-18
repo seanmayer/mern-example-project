291729bdb06de583e9a6ea306515841dbf0f3c9b
Sean Mayer <2008sean@live.com>
2022-01-18 06:14:17 +0000

    Added dev tools and middleware using body-parser package

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# mern-example-project
+# MERN Project Example
 Building a fullstack React.js application with Node.js, Express.js &amp; MongoDB (MERN)
 
-# Setting up NodeJS with express
+## Setting up NodeJS with express
 
 Install express -> `npm install express --save --prefix nodejs`
-
 Run the server -> `node nodejs/app.js`
+
+## Nodemon - NodeJS tool to allow project recompilations on save
+Install nodemon -> `npm install nodemon --save-dev --prefix nodejs`
+
+`package.json`:
+
+```
+{
+  "name": "express-nodejs",
+  "version": "1.0.0",
+  "description": "",
+  "main": "app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon app.js"
+}
+```
+
+Run nodemon: `npm start --prefix nodejs`
+
+## Body-parser - Ready to use middleware to parse incoming request bodys
+Install body-parser -> `npm install body-parser --save  --prefix nodejs`
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,28 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
-    let body = '';
-    req.on('end', () => {
-        const userName = body.split("=")[1];
-        if (userName) {
-            req.body = { name: userName };
-        }
-        next();
-    });
-    
-    req.on('data', chunk => {
-        body += chunk;
-    });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/user',(req, res, next) => {
+    res.send('<h1>Users:' + req.body.username + '</h1>');
 });
 
-app.use((req, res, next) => {
-    if(req.body) {
-        return res.send('<h1>' + req.body.name + '</h1>');
-    }
-
-    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
+app.get('/',(req, res, next) => {
+    res.send(
+        '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    );
 });
 
 app.listen(5000);
