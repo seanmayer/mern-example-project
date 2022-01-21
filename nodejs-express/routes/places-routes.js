a702f4faaad5058e15cf8c784835103887634a6c
Sean Mayer <2008sean@live.com>
2022-01-21 06:48:50 +0000

    Error handling

--- nodejs-express/routes/places-routes.js
+++ nodejs-express/routes/places-routes.js
@@ -22,6 +22,13 @@ router.get('/:pid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
+    
+    if(!place) {
+        const error = new Error('Could not find a place for the provided id.')
+        error.code = 404;
+        throw error;
+    }
+
     res.json({ place });
 });
 
@@ -30,6 +37,13 @@ router.get('/user/:uid',(req,res,next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.creator === userId;
     });
+
+    if(!place) {
+        const error = new Error('Could not find a place for the provided user id.')
+        error.code = 404;
+        return next(error);
+    }
+
     res.json({ place });
 
 });
