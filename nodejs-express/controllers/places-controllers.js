f31420bcc4b356dbcda4f57e2d882b9981826a47
Sean Mayer <2008sean@live.com>
2022-01-23 09:05:44 +0000

    Added Controllers

---
+++ nodejs-express/controllers/places-controllers.js
@@ -0,0 +1,50 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Tokyo',
+        description: 'The capital of Japan',
+        imageUrl: 'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2016/06/Tokyo-Tower--1024x683.jpg',
+        address: '4 Chome-2-8 Shibakoen, Minato City, Tokyo 105-0011, Japan',
+        location:{
+            lat:35.6585805,
+            lng:139.7454329
+        },
+        creator: 'u1'
+    }
+];
+
+const getPlaceById = (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+    
+    if(!place) {
+        throw new HttpError('Could not find a place for the provided id.', 404);
+    }
+
+    res.json({ place });
+};
+
+const getPlaceByUserId = (req,res,next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+
+    if(!place) {
+        return next(
+            new HttpError('Could not find a place for the provided user id.',404)
+        );
+    }
+
+    res.json({ place });
+
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
+
+
--- nodejs-express/routes/places-routes.js
+++ nodejs-express/routes/places-routes.js
@@ -1,51 +1,11 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controllers')
+
 const router = express.Router();
 
+router.get('/:pid', placesControllers.getPlaceById);
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Tokyo',
-        description: 'The capital of Japan',
-        imageUrl: 'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2016/06/Tokyo-Tower--1024x683.jpg',
-        address: '4 Chome-2-8 Shibakoen, Minato City, Tokyo 105-0011, Japan',
-        location:{
-            lat:35.6585805,
-            lng:139.7454329
-        },
-        creator: 'u1'
-    }
-];
-
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
-    
-    if(!place) {
-        throw new HttpError('Could not find a place for the provided id.', 404);
-    }
-
-    res.json({ place });
-});
-
-router.get('/user/:uid',(req,res,next) => {
-    const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    });
-
-    if(!place) {
-        return next(
-            new HttpError('Could not find a place for the provided user id.',404)
-        );
-    }
-
-    res.json({ place });
-
-});
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 module.exports = router;
