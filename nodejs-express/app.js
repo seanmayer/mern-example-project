5e54ead323deab73da40054c55fa5facb9486b7d
Sean Mayer <2008sean@live.com>
2022-01-25 07:03:50 +0000

    Setting up the user routes, signup, login, getusers

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
-const HttpError = require('./models/http-error')
+const userRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
+app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route',404);
--- nodejs-express/controllers/places-controllers.js
+++ nodejs-express/controllers/places-controllers.js
@@ -30,19 +30,19 @@ const getPlaceById = (req, res, next) => {
     res.json({ place });
 };
 
-const getPlaceByUserId = (req,res,next) => {
+const getPlacesByUserId = (req,res,next) => {
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(p => {
+    const places = DUMMY_PLACES.filter(p => {
         return p.creator === userId;
     });
 
-    if(!place) {
+    if(!places || places.length === 0) {
         return next(
-            new HttpError('Could not find a place for the provided user id.',404)
+            new HttpError('Could not find a places for the provided user id.',404)
         );
     }
 
-    res.json({ place });
+    res.json({ place: places });
 
 };
 
@@ -87,6 +87,6 @@ exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 
 
