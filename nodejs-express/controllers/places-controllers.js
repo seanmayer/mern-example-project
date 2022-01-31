b7cd458b1307db39610c07448bc0c38da943982c
Sean Mayer <2008sean@live.com>
2022-01-31 06:19:01 +0000

    Validating patch requests and user routes

--- nodejs-express/controllers/places-controllers.js
+++ nodejs-express/controllers/places-controllers.js
@@ -1,6 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
-
 const HttpError = require('../models/http-error');
 
 let DUMMY_PLACES = [
@@ -50,7 +49,6 @@ const getPlacesByUserId = (req,res,next) => {
 const createPlace = (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        console.log(errors);
         throw new HttpError('Invalid inputs passed.', 422)
     }
 
@@ -70,6 +68,11 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        throw new HttpError('Invalid inputs passed.', 422)
+    }
+
     const { title, description } = req.body;
     const placeId = req.params.pid;
 
@@ -86,6 +89,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
+    if(!DUMMY_PLACES.find(p => p.id === placeId)) {
+        throw new HttpError('Could not find a place for that id.', 404)
+    }
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
     res.status(200).json({ message: 'Deleted place.' });
 };
