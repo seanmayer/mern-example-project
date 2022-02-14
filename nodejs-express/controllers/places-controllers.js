13d1e4b50babb172f3a461018a77b72880beaabc
Sean Mayer <2008sean@live.com>
2022-02-14 05:51:50 +0000

    Update place

--- nodejs-express/controllers/places-controllers.js
+++ nodejs-express/controllers/places-controllers.js
@@ -102,7 +102,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json(createdPlace);
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed.", 422);
@@ -111,14 +111,25 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id == placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch(err) {
+    const error = new HttpError("Could not update place", 500);
+    return next(error);
+  };
+  
+  place.title = title;
+  place.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError("Could not update place", 500);
+    return next(error);
+  }
 
-  res.status(200).json({ place: updatedPlace });
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
