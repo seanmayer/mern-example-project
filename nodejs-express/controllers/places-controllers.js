e8801f54adbfcf4f863fb3c309aaffd755f7ad41
Sean Mayer <2008sean@live.com>
2022-02-14 06:00:19 +0000

    removed redudent const

--- nodejs-express/controllers/places-controllers.js
+++ nodejs-express/controllers/places-controllers.js
@@ -4,22 +4,6 @@ const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Tokyo",
-    description: "The capital of Japan",
-    imageUrl:
-      "https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2016/06/Tokyo-Tower--1024x683.jpg",
-    address: "4 Chome-2-8 Shibakoen, Minato City, Tokyo 105-0011, Japan",
-    location: {
-      lat: 35.6585805,
-      lng: 139.7454329,
-    },
-    creator: "u1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   let place;
   try {
