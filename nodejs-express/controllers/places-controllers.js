d716d4a12706098139bfca686da7269050fcee9c
Sean Mayer <2008sean@live.com>
2022-02-25 20:56:07 +0000

    Deleting a place and removing if from a user

--- nodejs-express/controllers/places-controllers.js
+++ nodejs-express/controllers/places-controllers.js
@@ -145,13 +145,24 @@ const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
   let place;
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) {
     const error = new HttpError("Something went wrong", 500);
     return next(error);
   }
+
+  if (!place) {
+    const error = new HttpError("Could not find place for this id",404);
+    return next(error);
+  }
+
   try {
-    await place.remove();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({session: sess});
+    place.creator.places.pull(place); // removes the place from the user too
+    await place.creator.save({session: sess});
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError("Something went wrong", 500);
     return next(error);
