78f8a1585c63ba9fc9cad9ea630789df7a4f7fc2
Sean Mayer <2008sean@live.com>
2022-03-07 12:02:54 +0000

    Getting Users with GET Request

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,49 @@
-import React from 'react';
-
-import UsersList from '../components/UsersList'
+import React, { useEffect, useState } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    const USERS = [
-        {
-            id:'u1',
-            name:'Sean',
-            image:
-                'https://scaleablethinking.files.wordpress.com/2021/12/498ba7d2-fd7c-46b5-af73-980efd3903e9_1_201_a.jpeg',
-            places: 3
-        }
-    ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-    return <UsersList items={USERS}/>;
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
