aa621ba91fa341d72bdf8e36d98096e1822b9b65
Sean Mayer <2008sean@live.com>
2022-03-07 05:42:22 +0000

    Sending a login request

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -58,10 +58,33 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again");
+      }
     } else {
-      setIsLoading(true);
       try {
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
@@ -79,7 +102,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
