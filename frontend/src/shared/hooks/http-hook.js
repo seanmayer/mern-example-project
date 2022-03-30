9a525d84925fd6c3f0eb3492466a0a287debf7d5
Sean Mayer <2008sean@live.com>
2022-03-30 06:16:13 +0100

    tidy up

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -47,7 +47,7 @@ export const useHttpClient = () => {
 
   useEffect(() => {
     return () => {
-      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
     };
   }, []);
 
