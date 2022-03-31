42e105228a9fe647f9c1dd319c679d4063ed53d2
Sean Mayer <2008sean@live.com>
2022-03-31 06:10:31 +0100

    Linking users to images

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -32,6 +33,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
