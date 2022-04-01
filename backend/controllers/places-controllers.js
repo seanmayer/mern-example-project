6ea33372ff4bd94556889301af7bf255bce9000c
Sean Mayer <2008sean@live.com>
2022-04-01 15:07:21 +0100

    Delete image file from backend

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const { validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 const HttpError = require("../models/http-error");
@@ -158,6 +160,8 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = place.image;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -169,6 +173,11 @@ const deletePlace = async (req, res, next) => {
     const error = new HttpError("Something went wrong", 500);
     return next(error);
   }
+
+  fs.unlink(imagePath, err => {
+    console.log(err);
+  });
+
   res.status(200).json({ message: "Deleted place." });
 };
 
