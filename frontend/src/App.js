e9528f395f8f11244b4d90b4a7341a3a26d6abef
Sean Mayer <2008sean@live.com>
2022-04-08 06:09:14 +0100

    Authentication - Persist Login LocalStorage token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,74 @@
-import React, { useState, useCallback } from 'react';
-import { 
-  BrowserRouter as Router, 
-  Route, 
-  Redirect, 
-  Switch
-} from 'react-router-dom';
+import React, { useState, useCallback, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Auth from './user/pages/Auth';
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import { AuthContext } from './shared/context/auth-context';
+import Auth from "./user/pages/Auth";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
-  
+
   const login = useCallback((uid, token) => {
     setToken(token);
     setUserId(uid);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
   let routes;
 
   if (token) {
     routes = (
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route> 
-      <Route path="/:userId/places" exact>
-        <UserPlaces />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route> 
-      <Route path="/places/:placeId">
-        <UpdatePlace />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     );
   } else {
     routes = (
       <Switch>
         <Route path="/" exact>
           <Users />
-        </Route> 
+        </Route>
         <Route path="/:userId/places" exact>
           <UserPlaces />
         </Route>
@@ -66,21 +81,21 @@ const App = () => {
   }
 
   return (
-    <AuthContext.Provider 
-      value={{ 
+    <AuthContext.Provider
+      value={{
         isLoggedIn: !!token,
-        token: token, 
-        userId: userId, 
-        login: login, 
-        logout: logout 
-        }}
-        >
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
       <Router>
         <MainNavigation />
         <main>
-            { routes }
+          { routes }
         </main>
-        </Router>
+      </Router>
     </AuthContext.Provider>
   );
 };
