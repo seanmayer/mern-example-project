8a039a6c9aefcd2280767443c823110b06f46a78
Sean Mayer <2008sean@live.com>
2022-04-13 19:46:29 +0100

    Deployment Ready - Node.js REST API

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cgogk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(() => {
-  app.listen(5000);
+  app.listen(process.env.PORT || 5000);
 }).catch((error) => {
   console.log(error);
 });
